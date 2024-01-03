e76852e87f417ddcbc8b36dc3dd515d183425bf1
Joydeep223 <127617816+Joydeep223@users.noreply.github.com>
2024-01-03 21:50:02 +0530

    Add files via upload

---
+++ q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter the radius";
+    float r;
+    cin>>r;
+    cout<<"enter the height";
+    float h;
+    cin>>h;
+    float v = 3.14*r*r*h;
+    cout<<"the volume of cylinder is:"<<v;
+
+
+}
---
+++ q5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter 1st charater";
+    char x;
+    cin>>x;
+    cout<<"enter 2nd charater";
+    char y;
+    cin>>y;
+     int z=(int)x-(int)y;
+     cout<<"the difference is: "<<z;
+
+
+}
